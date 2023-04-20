e54efb7915a8f8e3e709b157fe4c985af9d6c5cf
Gabriel Waksmundzki <czapek1337@gmail.com>
2023-04-20 22:10:22 +0200

    * Add a proper run.sh for Linux hosts

--- run.sh
+++ run.sh
@@ -1,3 +1,9 @@
-# works only in wsl :)
-
-cmd.exe /k "run.bat && exit"
+qemu-system-x86_64 \
+    -no-reboot \
+    -no-shutdown \
+    -M q35 \
+    -m 256M \
+    -smp 4 \
+    -boot d \
+    -cdrom build/image.iso \
+    -debugcon stdio
